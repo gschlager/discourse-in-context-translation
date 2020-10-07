5b2a63e0ff960154bf60583e56392fa0446b9a06
Gerhard Schlager <gerhard.schlager@discourse.org>
2020-10-08 01:22:45 +0200

    First test of Crowdin in-context translation

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# name: discourse-in-context-translation
+# about: Allows in-context translation of Discourse using Crowdin.
+# version: 1.0
+
+register_locale("en_IC", name: "In-Context Translation", nativeName: "In-Context Translation", fallbackLocale: "en")
