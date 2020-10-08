44923a5fdd72038b5124dea227cf608ce857145d
Gerhard Schlager <gerhard.schlager@discourse.org>
2020-10-09 01:32:07 +0200

    Crowdin JS was missing in wizard and CSP was somehow not working

--- plugin.rb
+++ plugin.rb
@@ -7,23 +7,28 @@
 register_locale("en_IC", name: "In-Context Translation", nativeName: "In-Context Translation", fallbackLocale: "en")
 register_asset "javascripts/crowdin.js"
 
-after_initialize do
-  CROWDIN_JS_URL = "https://cdn.crowdin.com/jipt/jipt.js"
+CROWDIN_JS_URL = "https://cdn.crowdin.com/jipt/jipt.js"
 
-  def add_before_head_html
-    <<~HTML
+def crowdin_js
+  <<~HTML
       <script type="text/javascript" src="#{CROWDIN_JS_URL}"></script>
-    HTML
-  end
+  HTML
+end
 
-  register_html_builder("server:before-head-close-crawler") { add_before_head_html }
-  register_html_builder("server:before-head-close") { add_before_head_html }
-  register_html_builder("wizard:head") { add_before_head_html }
+register_html_builder("server:before-head-close-crawler") { crowdin_js }
+register_html_builder("server:before-head-close") { crowdin_js }
+register_html_builder("wizard:head") do |controller|
+  <<~HTML
+      #{controller.helpers.preload_script("plugins/discourse-in-context-translation")}
+      #{crowdin_js}
+  HTML
+end
 
-  extend_content_security_policy(
-    script_src: [CROWDIN_JS_URL, "unsafe-eval"]
-  )
+extend_content_security_policy(
+  script_src: [CROWDIN_JS_URL, "unsafe-eval"]
+)
 
+after_initialize do
   # It's not really hidden, but this prevents changing the default_locale anyway.
   # That's good enough for now.
   SiteSetting.hidden_settings.concat([:default_locale])
